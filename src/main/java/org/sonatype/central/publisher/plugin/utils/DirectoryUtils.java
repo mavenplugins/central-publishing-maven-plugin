039f16633a0fdf26ffee5a66276edd5f78da5773
Markus Hoffrogge <mhoffrogge@gmail.com>
2025-10-04 19:40:08 +0200

    Integrate changes of original version 0.9.0
    - update java sources from
      https://repo1.maven.org/maven2/org/sonatype/central/central-publishing-maven-plugin/0.9.0/central-publishing-maven-plugin-0.9.0-sources.jar
      - ArtifactStagerImpl.java:
        - precise logging per artifact
      - PublishMojo.java:
        - consider skipPublishing per module
      - DirectoryUtils.java:
        - move method hasFiles(final File directory)
          from PublishMojo.java into this util class
      - format code via spotbugs:apply

---
+++ src/main/java/org/sonatype/central/publisher/plugin/utils/DirectoryUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022-present Sonatype, Inc. All rights reserved.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package org.sonatype.central.publisher.plugin.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class DirectoryUtils
+{
+  /**
+   * Checks if the given directory contains any files.
+   *
+   * @param directory the directory to check
+   * @return true if the directory contains files, false otherwise
+   */
+  public static boolean hasFiles(final File directory) {
+    try {
+      if (directory.exists()) {
+        Path path = directory.toPath();
+        if (Files.exists(path) && Files.isDirectory(path)) {
+          try (Stream<Path> paths = Files.list(path)) {
+            return paths.findFirst().isPresent();
+          }
+        }
+      }
+    }
+    catch (IOException ignored) {
+    }
+    return false;
+  }
+}
